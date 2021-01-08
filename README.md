d3eb4875116cdbdfcbc75f66c802320e7f2cdf2a
Donghyeong <ehdgud31@uos.ac.kr>
2021-01-08 19:03:04 +0900

    backend db configuration

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # ABAO
 
-앞으로 쓰게 될 react 스택에 대해 간단한 ! 예제 입니다.
+앞으로 쓰게 될 개발 스택에 대해 간단한 ! 설명 & 예제 입니다.
+
+### Frontend
+우선 프론트엔드는 javascript에 typescript를 적용한 React 라이브러리를 이용해 작성할 것이고,
+Create-react-app을 이용해서 배포할 것입니다.
+css 스타일링은 sass, 기본적인 컴포넌트들은 material-ui 이용해서 작성하고
+상태관리는 React Hook을 이용하며 서버와 API 통신할 때는 axios 라이브러리를 이용합니다.
+
+### Server
+
+백엔드 서버도 javascript + typescript를 적용한 nodejs Express 프레임워크로 구성되고
+데이터베이스는 NoSQL인 Mongodb, 디비 코드 작성을 위해 Mongoose 라이브러리를 이용합니다.
 
 ### Structure
 - /backend : node + express 로 구현한 API 서버.
@@ -9,7 +20,7 @@
 
 기본적으로 node 2개(API서버, 프론트) + nginx + DB 총 4개 컨테이너 띄워서 통신하도록 하였습니당.
 
-80 포트로 접근하면 내부적으로 프론트 또는 서버로 라우팅 합니다.
+80 포트로 접근하면 nginx가 프론트 또는 API 서버로 라우팅 합니다.
 
 예를 들어 http://localhost 로 접속 시 리액트 서버, http:/localhost/api 로 접근시 API 서버
 
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,8 +2,7 @@ import express, { urlencoded } from 'express';
 import cors from 'cors';
 import * as bodyParser from 'body-parser';
 import api from './api';
-
-import { API_PORT } from '../../constants';
+import mongoose from 'mongoose';
 class App {
   app: express.Application;
 
@@ -18,6 +17,14 @@ class App {
     this.app.use(bodyParser.urlencoded({extended:true}));
     this.app.use(bodyParser.json());
     // TODO : DB CONFIG
+    const connect = mongoose.connect('mongodb://mongo/admin',{
+      useNewUrlParser: true,
+      user: "root2",
+      pass: "root",
+      dbName: 'abaotest',
+
+    },()=>{console.log("DB CONNECTED")});
+    mongoose.connection.on('error', (err) => {console.log(err)});
   }
 
   private middleware() {
@@ -25,10 +32,11 @@ class App {
   }
 
   private route() {
-    this.app.use('/api', api);
+    this.app.use('/', api);
   }
 }
 
 const app = new App().app;
 
+const API_PORT = 3001;
 app.listen(API_PORT, () => { console.log(`LISTEN ON PORT ${API_PORT}`)});
