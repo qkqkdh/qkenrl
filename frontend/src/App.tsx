2f43b7f0ff70023f123d2f824c21cf59c73574f5
heeeun <01072417732w@gmail.com>
2021-01-26 00:16:15 +0900

    set eslint, prettier in both folder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Register from './components/Register';
 import UserList from './components/UserList';
+
 const App: React.FunctionComponent = () => {
   return (
     <>
-    <Register />
-    <UserList />
+		<Register />
+		<UserList />
     </>
   )
 }
