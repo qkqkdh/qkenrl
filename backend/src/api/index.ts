6429428a6df04b524464ebe409fe0bb28eede454
heeeun <01072417732w@gmail.com>
2021-01-28 01:02:52 +0900

    make backend works in eslint

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -1,9 +1,8 @@
-import { Router } from 'express';
-import auth from './auth';
+import { Router } from "express";
+import auth from "./auth";
 
 const router = Router();
 
-router.use('/auth', auth);
-
+router.use("/auth", auth);
 
 export default router;
