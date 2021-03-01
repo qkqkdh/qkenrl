8f753d6a23eebeb8485eec297226558f94efdc03
MOBUMIN <kiju23@naver.com>
2021-03-01 18:50:19 +0900

    login modal 구현중
    * login modal 윗부분 구현

--- frontend/src/components/SignIn.tsx
+++ frontend/src/components/SignIn.tsx
@@ -1,14 +1,15 @@
-import React from "react";
-import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-import Link from "@material-ui/core/Link";
-import Paper from "@material-ui/core/Paper";
-import Box from "@material-ui/core/Box";
-import Grid from "@material-ui/core/Grid";
+import React, { useState } from "react";
+import { Avatar,
+	Button,
+	TextField,
+	Link,
+	Grid,
+	Typography,
+	Modal,
+	IconButton,
+} from "@material-ui/core";
 import PetsIcon from "@material-ui/icons/Pets";
-import Typography from "@material-ui/core/Typography";
+import CloseIcon from '@material-ui/icons/Close';
 
 function Copyright() {
 	return (
@@ -32,104 +33,51 @@ function ContactUs() {
 	);
 }
 
+function getModalStyle() {
+	const top = 50;
+	const left = 50;
+	return {
+		top: `${top}%`,
+		left: `${left}%`,
+		transform: `translate(-${top}%, -${left}%)`,
+	};
+}
+
 export default function SignInSide() {
+	const [Open, setOpen] = useState(false);
+
+	const handleOpen = () => {
+		setOpen(true);
+	};
+	const handleClose = () => {
+		setOpen(false);
+	};
+
+	const body = (
+		<div className="loginBody" style={getModalStyle()}>
+			<div className="rightSort">
+				<IconButton className="closeBtn">
+					<CloseIcon fontSize="small" />
+				</IconButton>
+			</div>
+			<Grid container className="loginContainer">
+				<Avatar className="avatar">
+					<PetsIcon fontSize="large" />
+				</Avatar>
+				<h2>Hi</h2>
+			</Grid>
+		</div>
+	);
 	return (
-		<Grid container component="main" className="root">
-			<CssBaseline />
-			<Grid
-				item
-				xs={false}
-				sm={4}
-				md={9}
-				className="image"
-				component={Paper}
-				elevation={6}
-				square
+		<div>
+			<Button onClick={handleOpen}>Login</Button>
+			<Modal
+				open={Open}
+				onClose={handleClose}
+				aria-labelledby="login-title"
 			>
-				<span>
-					<h1>한눈에 보는 당신의 반려동물</h1>
-					..이 들어갈 예정
-				</span>
-			</Grid>
-			<Grid
-				item
-				className="paperBG"
-				xs={12}
-				sm={8}
-				md={3}
-				component={Paper}
-				elevation={6}
-				square
-			>
-				<div className="paper">
-					<Avatar className="avatar">
-						<PetsIcon fontSize="large" />
-					</Avatar>
-					<h1>FIT PET</h1>
-					<form className="form" noValidate>
-						<TextField
-							variant="outlined"
-							margin="normal"
-							required
-							fullWidth
-							id="ID"
-							label="ID"
-							name="ID"
-							autoComplete="ID"
-							autoFocus
-						/>
-						<TextField
-							variant="outlined"
-							margin="normal"
-							required
-							fullWidth
-							name="password"
-							label="Password"
-							type="password"
-							id="password"
-							autoComplete="current-password"
-						/>
-						<Button
-							type="submit"
-							fullWidth
-							variant="contained"
-							color="primary"
-							size="large"
-							className="submit"
-						>
-							Sign In
-						</Button>
-						<Grid container justify="center" spacing={2}>
-							<Grid item>
-								<Link href="# " variant="body2">
-									회원가입
-								</Link>
-							</Grid>
-							<Grid item>|</Grid>
-							<Grid item>
-								<Link href="# " variant="body2">
-									비밀번호 찾기
-								</Link>
-							</Grid>
-						</Grid>
-					</form>
-				</div>
-				<Box mt={4}>
-					<Copyright />
-					<br />
-					<ContactUs />
-				</Box>
-			</Grid>
-			<Button
-				type="submit"
-				variant="contained"
-				color="primary"
-				size="large"
-				className="btn"
-				href="# "
-			>
-				둘러보기
-			</Button>
-		</Grid>
+				{body}
+			</Modal>
+		</div>
 	);
 }
