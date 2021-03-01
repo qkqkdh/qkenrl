ff202d0a5bc927d2956f272f955fc02682fa1e00
heeeun <gmldms784@naver.com>
2021-03-01 19:55:54 +0900

    complete smallplaceinfo

---
+++ frontend/src/pages/PlaceInfoPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Grid } from '@material-ui/core';
+
+const PlaceInfoPage : React.FC = () => {
+	const a = 1;
+	return (
+		<Grid>hi</Grid>
+	);
+};
+
+export default PlaceInfoPage;
