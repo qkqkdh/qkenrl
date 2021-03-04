b65fa4c09fc8e029166d98f0f7eca85ec7250d45
MOBUMIN <kiju23@naver.com>
2021-03-04 16:59:05 +0900

    introduction 돌려둠

--- frontend/src/pages/Introduction.tsx
+++ frontend/src/pages/Introduction.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 
-import { Intro, SignIn } from "../components";
+import { Intro } from "../components";
 import "../css/Introduction.scss";
 
 const Introduction: React.FunctionComponent = () => (
 	<>
 		<Intro />
-		<SignIn />
 	</>
 );
 
