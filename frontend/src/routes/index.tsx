935e10260d6b67fbde22c7798390ae784a12687d
LeeMir <manofgodmj@naver.com>
2021-03-14 23:41:40 +0900

    fix : overlapping

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { Login, PlacePage, PlaceInfoPage, NotFound, Main } from "../pages";
 import { Introduction, PlacePage, PlaceInfoPage, NotFound, Main } from "../pages";
 
 const Router: React.FunctionComponent = () => (
 	<BrowserRouter>
 		<Switch>
-      <Route exact path="/" component={Introduction} />
+			<Route exact path="/" component={Introduction} />
 			<Route path="/main" component={Main} />
 			<Route path="/place" component={PlacePage} />
 			<Route path="/place/:id" component={PlaceInfoPage} />
