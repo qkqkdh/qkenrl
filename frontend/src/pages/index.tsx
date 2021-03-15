8d4ad7db6959f027454ca83febf68ff53fe56746
LeeMir <manofgodmj@naver.com>
2021-03-15 21:06:47 +0900

    Update 'index.tsx's one more...

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,5 @@
 export { default as Introduction } from "./Introduction";
 export { default as SetPet } from "./SetPet";
-export { default as PetCustom } from "./PetCustom";
 export { default as Account } from "./Account";
 export { default as NotFound } from "./404";
 export { default as Main } from './Main';
