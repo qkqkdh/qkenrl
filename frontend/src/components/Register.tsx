6e2706ecf37e634f52973fa74a94735a80beef70
MOBUMIN <kiju23@naver.com>
2021-03-15 22:41:15 +0900

    first step 완료

--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -15,6 +15,8 @@ import {
 	Paper,
 	Radio,
 	RadioGroup,
+	FormControl,
+	FormControlLabel,
 } from "@material-ui/core";
 import CloseIcon from '@material-ui/icons/Close';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
@@ -44,25 +46,45 @@ const getModalStyle = () => {
 const getSteps = () => ['약관동의', '정보입력', '이메일 인증'];
 
 function getStepContent(step:number) {
+	const [value, setValue] = useState('disagree');
+	const valueHandle = (e:any) => {
+		setValue(e.target.value);
+	};
+	const [value2, setValue2] = useState('disagree');
+	const value2Handle = (e:any) => {
+		setValue2(e.target.value);
+	};
 	const agreeContent = (
 		<>
-			<p>제 1조. 로렘입숨</p>
-			<p>내용</p>
-			<p>제 2조. 로렘입숨</p>
-			<p>내용</p>
+			<p>제2항과 제3항의 처분에 대하여는 법원에 제소할 수 없다. 피고인의 자백이 고문·폭행·협박·구속의 부당한 장기화 또는 기망 기타의 방법에 의하여 자의로 진술된 것이 아니라고 인정될 때 또는 정식재판에 있어서 피고인의 자백이 그에게 불리한 유일한 증거일 때에는 이를 유죄의 증거로 삼거나 이를 이유로 처벌할 수 없다.</p>
+			<p>국가안전보장회의는 대통령이 주재한다. 국가안전보장회의의 조직·직무범위 기타 필요한 사항은 법률로 정한다. 국회의원의 수는 법률로 정하되, 200인 이상으로 한다.</p>
+			<p>헌법재판소는 법률에 저촉되지 아니하는 범위안에서 심판에 관한 절차, 내부규율과 사무처리에 관한 규칙을 제정할 수 있다. 모든 국민은 신체의 자유를 가진다. 누구든지 법률에 의하지 아니하고는 체포·구속·압수·수색 또는 심문을 받지 아니하며, 법률과 적법한 절차에 의하지 아니하고는 처벌·보안처분 또는 강제노역을 받지 아니한다.</p>
 		</>
 	);
 	switch (step) {
 	case 0:
 		return (
 			<>
-				<Typography>회원 이용 약관</Typography>
-				<Paper variant="outlined">{agreeContent}</Paper>
-				<Typography>동의하십니까?</Typography>
-				<Checkbox />
-				<Typography>개인정보수집동의에 대한 고지사항</Typography>
-				<Paper variant="outlined">{agreeContent}</Paper>
-				<Typography>동의하십니까?</Typography>
+				<Grid container className="stepzero-grid">
+					<Typography className="register-subhead">회원 이용 약관</Typography>
+					<Paper variant="outlined" className="agree-content">{agreeContent}</Paper>
+					<FormControl className="radio-content">
+						<Typography variant="body2">동의하십니까?</Typography>
+						<RadioGroup value={value} onChange={valueHandle} className="radio-group">
+							<FormControlLabel value="agree" control={<Radio color="default" size="small" />} label={<Typography variant="body2">예</Typography>} />
+							<FormControlLabel value="disagree" control={<Radio color="default" size="small" />} label={<Typography variant="body2">아니오</Typography>} />
+						</RadioGroup>
+					</FormControl>
+					<Typography className="register-subhead">개인정보수집동의에 대한 고지사항</Typography>
+					<Paper variant="outlined" className="agree-content">{agreeContent}</Paper>
+					<FormControl className="radio-content">
+						<Typography variant="body2">동의하십니까?</Typography>
+						<RadioGroup value={value2} onChange={value2Handle} className="radio-group">
+							<FormControlLabel value="agree" control={<Radio color="default" size="small" />} label={<Typography variant="body2">예</Typography>} />
+							<FormControlLabel value="disagree" control={<Radio color="default" size="small" />} label={<Typography variant="body2">아니오</Typography>} />
+						</RadioGroup>
+					</FormControl>
+				</Grid>
 			</>
 		);
 	case 1:
@@ -138,7 +160,7 @@ const Register: React.FunctionComponent<Props> = ({ children }) => {
 							</Step>
 						))}
 					</Stepper>
-					<Typography>{getStepContent(activeStep)}</Typography>
+					{getStepContent(activeStep)}
 					<Grid container className="register-btngroup" spacing={2}>
 						<Grid item className="register-btn">
 							<Button onClick={Closehandler} variant="outlined" fullWidth>취소</Button>
