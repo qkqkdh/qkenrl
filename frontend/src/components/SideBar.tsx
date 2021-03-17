d565a3d80c2f18ecc8480587c50472f986d6e79f
heeeun <01072417732w@gmail.com>
2021-03-17 18:02:40 +0900

    mobile sidebar

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-import { Grid, Menu, MenuItem, Drawer } from "@material-ui/core";
-import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
+import { Drawer } from "@material-ui/core";
+
+import { Header } from ".";
 
 type Props = {
 	open: boolean;
@@ -29,46 +30,7 @@ const SideBar = ({ open, handleSideBarClose }: Props) => {
 			onClose={handleSideBarClose}
 			className="side-bar"
 		>
-			<Grid className="nav-con">
-				<span>
-					<Link to="/main">지도</Link>
-				</span>
-				<span>
-					<Link to="/search">기능 1</Link>
-				</span>
-				<span>
-					<Link to="/pets">기능 2</Link>
-				</span>
-			</Grid>
-			<Grid className="profile-con">
-				<p>
-					<strong>{name}</strong>
-					님, 환영합니다!
-				</p>
-				<button
-					className="menu-btn"
-					type="button"
-					onClick={(e) => handleClick(e)}
-				>
-					<KeyboardArrowDownIcon />
-				</button>
-				<Menu
-					id="simple-menu"
-					anchorEl={anchorEl}
-					getContentAnchorEl={null}
-					anchorOrigin={{
-						vertical: 'bottom',
-						horizontal: 'center',
-					}}
-					keepMounted
-					open={Boolean(anchorEl)}
-					onClose={handleClose}
-				>
-					<MenuItem onClick={handleClose}>Profile</MenuItem>
-					<MenuItem onClick={handleClose}>My account</MenuItem>
-					<MenuItem onClick={handleClose}>Logout</MenuItem>
-				</Menu>
-			</Grid>
+			<Header />
 		</Drawer>
 	);
 };
