09c229881e9774b1708f5bff62c7fb363533537a
heeeun <gmldms784@naver.com>
2021-03-30 09:43:23 +0900

    make heart style for placeinfo

--- frontend/src/components/PlaceInfo.tsx
+++ frontend/src/components/PlaceInfo.tsx
@@ -62,9 +62,11 @@ const PlaceInfo = ({ size, place }: PlaceProps) => {
 						{
 							place.isMyPlace ?
 								<FavoriteIcon
+									className="myPlace"
 									onClick={switchMyPlace}
 								/> :
 								<FavoriteBorderIcon
+									className="notMyPlace"
 									onClick={switchMyPlace}
 								/>
 						}
